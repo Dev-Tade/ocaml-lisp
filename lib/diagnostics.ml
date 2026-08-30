4cc9b3d0c18a1918f3dae20e1e7d068506d07c55
Dev-Tade <88458312+Dev-Tade@users.noreply.github.com>
2026-08-30 19:41:41 -0300

    :make proper interface for diagnostics, hide errors behind utility functions to raise them

--- lib/diagnostics.ml
+++ lib/diagnostics.ml
@@ -8,6 +8,9 @@ module Location = struct
 
   type 'a located = 'a * t 
 
+  let make file line column =
+    { source = file; line; column; }
+
   let to_string (loc : t) : string =
     Printf.sprintf "%s:%d:%d" 
       loc.source 
@@ -18,20 +21,43 @@ module Location = struct
     print_string (to_string loc)
 end
 
-module Error = struct
+module Errors = struct
   type t =
     (* General Purpose *)
     | Unreacheable of Location.t * string
-    (* Lexer Errors *)
+    (* Lexer *)
     | Unexpected_Character of Location.t * char
-    (* Parser Errors *)
+    (* Parser *)
     | Unexpected_Token of Location.t * string
     | Unexpected_EndOfInput of Location.t * string
-    (* Evaluator/Runtime Errors *)
+    (* Evaluator/Runtime *)
     | Runtime_Conversion of Location.t * string
     | Unbound_Symbol of Location.t * string
     | Not_Applicable of Location.t * string
-    | Arity_Mismatch of Location.t * string * string * int
+    | Arity_Mismatch of Location.t * string * string list * string list
+
+  exception Error of t
+
+  let unreacheable loc where = 
+    raise (Error (Unreacheable (loc, where)))
+  
+  let unexpected_character loc character = 
+    raise (Error (Unexpected_Character (loc, character))) 
+  
+  let unexpected_token loc token_string = 
+    raise (Error (Unexpected_Token (loc, token_string)))
+  
+  let unexpected_eof loc what =
+    raise (Error (Unexpected_EndOfInput (loc, what)))
+  
+  let unbound_symbol loc symbol = 
+    raise (Error (Unbound_Symbol (loc, symbol)))
+
+  let not_applicable loc what =
+    raise (Error (Not_Applicable (loc, what)))
+    
+  let arity_mismatch loc func args got =
+    raise (Error (Arity_Mismatch (loc, func, args, got)))
 
   let to_string (msg : t) : string =
     match msg with
@@ -70,10 +96,20 @@ module Error = struct
         (Location.to_string loc)
         reason
 
-    | Arity_Mismatch (loc, name, args, got) ->
-      Printf.sprintf "%s: Arity mismatch, %s %s, but got %d"
-        (Location.to_string loc)
-        name args got
-end
+    | Arity_Mismatch (loc, name, args, gots) ->
+      let suffix = function
+      | x when x != 1 -> "arguments"
+      | _ -> "argument"
+      in
 
-exception Error of Error.t
+      let args_count = List.length args in
+      let gots_count = List.length gots in
+      let miss_count = args_count - gots_count in
+
+      Printf.sprintf "%s: Arity mismatch %s expects %d %s: %s, but got %d %s: %s. Missing %d %s: %s"
+      (Location.to_string loc) name 
+      args_count (suffix args_count) (String.concat ", " args)
+      gots_count (suffix gots_count) (String.concat ", " gots)
+      miss_count (suffix miss_count) (String.concat ", " 
+        (List.drop miss_count args))
+end
---
+++ lib/diagnostics.mli
@@ -0,0 +1,30 @@
+module Location : sig
+  type t = { line : int; column : int; source : string; }
+  type 'a located = 'a * t
+  
+  val make : string -> int -> int -> t 
+  
+  val to_string : t -> string
+  val print : t -> unit
+end
+
+
+module Errors : sig
+    type t
+    exception Error of t
+    
+    val to_string : t -> string
+
+    (* General Purpose *)
+    val unreacheable : Location.t -> string -> 'a
+    (* Lexer *)
+    val unexpected_character : Location.t -> char -> 'a
+    (* Parser *)
+    val unexpected_token : Location.t -> string -> 'a
+    val unexpected_eof : Location.t -> string -> 'a
+    (* Evaluator/Runtime *)
+    val unbound_symbol : Location.t -> string -> 'a
+    val not_applicable : Location.t -> string -> 'a
+    val arity_mismatch : Location.t -> string -> string list -> string list -> 'a
+
+  end
